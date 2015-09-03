2e90dd79c7524911c72bab0e5afc0aa7d538a283
myusername <anju.shahi@deerwalk.edu.np>
2015-09-03 12:37:35 +0545

    First commit

---
+++ calculator/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float p = Convert.ToInt32(Convert.ToDecimal(textBox1.Text));
+            float t = Convert.ToInt32(Convert.ToDecimal(textBox1.Text));
+            float r = Convert.ToInt32(Convert.ToDecimal(textBox1.Text));
+     
+            calculate c = new calculate(p,t,r);
+            float ans = c.cal();
+
+            label1.Text = ans.ToString();
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
---
+++ calculator/calculate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calculator
+{
+    class calculate
+    {
+            float p;
+            float t;
+            float r;
+
+            public calculate(float x,float y,float z)
+            {
+                this.p = x;
+                this.t = y;
+                this.r = z;
+            }
+
+
+            public float cal()
+            {
+                return ((p * t * r) / 100);
+            }
+
+            
+    }
+}
